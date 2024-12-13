e0a7fd8e066d821cb84bf693470edac7456ec864
Holub Patrik <23bulohp@gmail.com>
2024-12-13 15:00:36 +0100

    Simplified build files - Added parameter window and set it to show it most of the time

--- nvim-manual/.config/nvim/lua/overseer/template/user/cpp/init.lua
+++ nvim-manual/.config/nvim/lua/overseer/template/user/cpp/init.lua
@@ -1,8 +1,4 @@
 return {
 	"user.cpp.build",
-	"user.cpp.build23",
-	"user.cpp.release",
-	"user.cpp.release23",
 	"user.cpp.debug",
-	"user.cpp.debug23",
 }
--- nvim-manual/.config/nvim/lua/plugins/overseer.lua
+++ nvim-manual/.config/nvim/lua/plugins/overseer.lua
@@ -6,6 +6,7 @@ local M = {
 				"builtin",
 				"user.cpp",
 			},
+			default_template_prompt = "missing",
 
 			vim.keymap.set("n", "<leader>r", "<cmd>:OverseerRun<cr>", {}),
 		})
