73c6afa0ccd80c706a7909df6a9c75f8386e1764
Holub Patrik <23bulohp@gmail.com>
2024-12-22 17:47:13 +0100

    Fixed conform, changed debug to take parameters

--- nvim-manual/.config/nvim/lua/configs/debug.lua
+++ nvim-manual/.config/nvim/lua/configs/debug.lua
@@ -37,7 +37,22 @@ dap.configurations.cpp = {
 		type = "codelldb",
 		request = "launch",
 		program = function()
-			return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+			local file
+			vim.ui.input({
+				prompt = "Path to executable: " .. vim.fn.getcwd() .. "/",
+			}, function(str)
+				file = vim.fn.getcwd() .. "/" .. str
+			end)
+			return file
+		end,
+		args = function()
+			local args
+			vim.ui.input({
+				prompt = "Arguments to use: ",
+			}, function(str)
+				args = vim.split(str, " ")
+			end)
+			return args
 		end,
 		cwd = "${workspaceFolder}",
 		stopOnEntry = false,
--- nvim-manual/.config/nvim/lua/plugins/conform.lua
+++ nvim-manual/.config/nvim/lua/plugins/conform.lua
@@ -20,19 +20,12 @@ return {
 		formatters_by_ft = {
 			lua = { "stylua" },
 			python = { "ruff" },
-			javascript = { "prettierd", "prettier", stop_after_first = true },
+			c = { lsp_format = "prefer" },
+			cpp = { lsp_format = "prefer" },
 		},
 		default_format_opts = {
 			lsp_format = "fallback",
 		},
-		format_on_save = { timeout_ms = 500 },
-		formatters = {},
+		format_on_save = { timeout_ms = 200 },
 	},
-	config = function()
-		require("conform").formatters.php = {
-			inherit = false,
-			command = "php-cs-fixer",
-			args = { "fix", "." },
-		}
-	end,
 }
