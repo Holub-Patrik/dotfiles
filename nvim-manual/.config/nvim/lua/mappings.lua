c9200b12377ecb7742b59e641a8cd716dca03341
Holub Patrik <23bulohp@gmail.com>
2025-03-16 15:00:44 +0100

    Trying to do floating scratch terminal

--- nvim-manual/.config/nvim/lua/mappings.lua
+++ nvim-manual/.config/nvim/lua/mappings.lua
@@ -10,3 +10,13 @@ vim.keymap.set("n", "<leader>ft", oil.toggle_float, {})
 vim.keymap.set("n", ";", ":", {})
 vim.keymap.set("n", "<leader>x", "a<esc><cr>", {})
 vim.keymap.set("n", "<leader>c", "a󰄬<esc><cr>", {})
+
+local function create_fterm()
+	local buffer = vim.api.nvim_create_buf(false, true)
+	vim.api.nvim_open_win(buffer, true,
+		{ relative = 'win', row = 5, col = 5, width = 100, height = 50, border = "rounded" })
+	local chan = vim.api.nvim_open_term(buffer, {})
+	vim.api.nvim_chan_send(chan, "zsh\n")
+end
+
+vim.keymap.set("n", "<leader>ot", create_fterm, {})
