7acc4a36c6c2afeb16361f5dc2f5e252e9ba3e5f
Holub Patrik <23bulohp@gmail.com>
2025-03-25 01:23:54 +0100

    Renamer and floating term

--- nvim-manual/.config/nvim/lua/mappings.lua
+++ nvim-manual/.config/nvim/lua/mappings.lua
@@ -12,15 +12,31 @@ vim.keymap.set("n", "<leader>x", "a<esc><cr>", {})
 vim.keymap.set("n", "<leader>c", "a󰄬<esc><cr>", {})
 vim.keymap.set("t", "<esc>", "<C-\\><C-n>")
 
+vim.api.nvim_create_autocmd("TermOpen", {
+	pattern = "*",
+	callback = function()
+		vim.cmd([[setlocal nonu nornu]])
+		vim.cmd([[startinsert]])
+	end,
+})
+
 local function create_fterm()
 	local buffer = vim.api.nvim_create_buf(false, true)
-	vim.api.nvim_open_win(
-		buffer,
-		true,
-		{ relative = "win", row = 5, col = 5, width = 100, height = 50, border = "rounded" }
-	)
-	local chan = vim.api.nvim_open_term(buffer, {})
-	vim.api.nvim_chan_send(chan, "zsh\n")
+	local window_width = vim.api.nvim_win_get_width(0)
+	local window_height = vim.api.nvim_win_get_height(0)
+
+	local ratio = 0.9
+	local complement_ratio = (1 - ratio) / 2
+
+	vim.api.nvim_open_win(buffer, true, {
+		relative = "win",
+		col = math.floor(complement_ratio * window_width),
+		row = math.floor(complement_ratio * window_height),
+		width = math.floor(ratio * window_width),
+		height = math.floor(ratio * window_height),
+		border = "rounded",
+	})
+	vim.fn.termopen("zsh")
 end
 
 vim.keymap.set("n", "<leader>ot", create_fterm, {})
---
+++ nvim-manual/.config/nvim/lua/plugins/renamer.lua
@@ -0,0 +1,8 @@
+return {
+	"filipdutescu/renamer.nvim",
+	event = "LspAttach",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		require("renamer").setup({})
+	end,
+}
