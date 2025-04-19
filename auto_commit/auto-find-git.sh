f511e574d4fb918448b7124636fb5068b2096864
Holub Patrik <23bulohp@gmail.com>
2025-04-20 00:53:44 +0200

    auto check if tree is clean

--- auto_commit/auto-find-git.sh
+++ auto_commit/auto-find-git.sh
@@ -4,7 +4,9 @@ pull() {
 
 push() {
 	git add ./
-	git commit -m "Holub Patrik auto-commit"
+	read -p "Please enter a commit msg: " msg
+	msg=${msg:- "Holub Patrik auto-commit"}
+	git commit -m "$msg"
 	git push
 }
 
@@ -14,27 +16,39 @@ for loc in $(find ~/ -type d -name ".git" 2>/dev/null |
 	grep -v ".*/\..*\.git" | 
 	grep -v "find.*" | 
 	sed -e 's/.git//'); do
+	
+	echo -e "\033[38;5;40mChecking: ${loc}\033[0m"
+	cd "$loc"
+	
+	if output=$(git status --porcelain) && ! [ -z "$output" ]; then
+		echo -e "\033[38;5;220mWorking Tree isn't clean\033[0m\n"
+		git status
 
-	read -p "Interact with: ${loc}? [y|n]: " interaction
-	interaction=${interaction:-n}
+		echo -n -e "\033[38;5;135mInteract with: ${loc}? [y|n]:\033[0m "
+		read -p "" interaction
+		interaction=${interaction:-n}
 
-	if [ "$interaction" = "y" ]; then
-		read -p "[P]ull/[p]ush? " command
-		command=${command:-P}
-		if [ "$command" = "P" ]; then
-			cd "$loc"
-			pull
-		elif [ "$command" = "p" ]; then
-			cd "$loc"
-			push
+		if [ "$interaction" = "y" ]; then
+
+			read -p "[P]ull/[p]ush? " command
+			command=${command:-P}
+
+			if [ "$command" = "P" ]; then
+				pull
+
+			elif [ "$command" = "p" ]; then
+				push
+
+			else
+				echo "Invalid command"
+			fi
+
+		elif [ "$interaction" = "n" ]; then
+			echo "continuing..."
 		else
-			echo "Invalid command"
+			echo "Invalid interaction"
 		fi
 
-	elif [ "$interaction" = "n" ]; then
-		echo "continuing..."
-	else
-		echo "Invalid interaction"
 	fi
 		
 done
