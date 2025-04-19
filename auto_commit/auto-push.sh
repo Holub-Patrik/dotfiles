4f72b3c3a855fe8f3da713731f8a209710fe1d25
Holub Patrik <23bulohp@gmail.com>
2025-04-19 23:48:38 +0200

    new commit from bash-script

--- auto_commit/auto-push.sh
+++ auto_commit/auto-push.sh
@@ -2,10 +2,22 @@
 
 git_repos=("$HOME/neorg-notes" "$HOME/dotfiles" "$HOME/school/kiv-ph")
 
+push() {
+	git add ./
+	git commit -m "new commit from bash-script"
+	git push
+}
+
+
 for repo in ${!git_repos[@]}; do
 	echo -e "\033[1m\033[38;5;40mPushing: ${git_repos[$repo]}\033[0m\n"
 	cd ${git_repos[$repo]}
-	git add ./
-	git commit -m "new commit from auto-commit"
-	git push
+	push
+done
+
+for repo_loc in "$@"
+do
+	echo -e "\033[1m\033[38;5;40mPushing: $repo_loc\033[0m\n"
+	cd "$repo_loc"
+	push
 done
