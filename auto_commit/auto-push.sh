e2b2d9e326f971e1dbc13ad8b29d5bb4b21c51a0
Holub Patrik <23bulohp@gmail.com>
2025-04-19 23:40:58 +0200

    new commit from auto-commit

--- auto_commit/auto-push.sh
+++ auto_commit/auto-push.sh
@@ -3,7 +3,7 @@
 git_repos=("$HOME/neorg-notes" "$HOME/dotfiles" "$HOME/school/kiv-ph")
 
 for repo in ${!git_repos[@]}; do
-	echo "Pushing: ${git_repos[$repo]}"
+	echo "\033b[1m\033b[38;5;{40}mPushing: ${git_repos[$repo]}\033b[0m\n"
 	cd ${git_repos[$repo]}
 	git add ./
 	git commit -m "new commit from auto-commit"
