0d9bdd889b39b21389f9c3d6cf8647df51195ff2
Holub Patrik <23bulohp@gmail.com>
2025-05-06 14:51:06 +0200

    fonts-ghostty

--- nvim-manual/.config/nvim/lua/options.lua
+++ nvim-manual/.config/nvim/lua/options.lua
@@ -20,6 +20,17 @@ vim.o.foldlevelstart = 99
 vim.o.foldenable = true
 vim.o.foldmethod = "expr"
 vim.o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+
+vim.api.nvim_create_autocmd("LspAttach", {
+	callback = function(args)
+		local client = vim.lsp.get_client_by_id(args.data.client_id)
+		if client and client:supports_method("textDocument/foldingRange") then
+			local win = vim.api.nvim_get_current_win()
+			vim.wo[win][0].foldexpr = "v:lua.vim.lsp.foldexpr()"
+		end
+	end,
+})
+
 vim.o.foldtext = require("configs.foldtext")
 
 vim.o.guifont = "monaspace_neon:h12"
