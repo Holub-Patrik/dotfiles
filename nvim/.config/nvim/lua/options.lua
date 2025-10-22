5bfea8819fd673e94f40d7993c5c4cbbe41930fc
Holub Patrik <23bulohp@gmail.com>
2025-10-22 22:43:36 +0200

    removed mkview and laodview autcmds, as they were causing more issues than they had use

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -15,20 +15,6 @@ vim.o.hlsearch = false
 vim.o.signcolumn = "yes"
 vim.o.guifont = "monaspace_neon:h12"
 
-vim.api.nvim_create_autocmd("BufWinLeave", {
-	pattern = "*.*",
-	callback = function()
-		vim.cmd("mkview")
-	end,
-})
-
-vim.api.nvim_create_autocmd("BufWinEnter", {
-	pattern = "*.*",
-	callback = function()
-		vim.cmd("silent! loadview")
-	end,
-})
-
 vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
 	pattern = { "*.norg" },
 	command = "set conceallevel=3",
