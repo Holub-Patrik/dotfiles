df1834f45771637e3f1239a741e6466599daea73
Holub Patrik <23bulohp@gmail.com>
2026-02-24 18:14:44 +0100

    Added dark theme setup stuffs, not yet complete though

---
+++ setup/darktheme.sh
@@ -0,0 +1,6 @@
+sudo pacman -S kvantum xdg-desktop-portal-gtk
+paru -S gnome-themes-extra gnome-themes-extra-gtk2
+gsettings set org.gnome.desktop.interface color-sheme 'prefer-dark'
+gsettings set org.gnome.desktop.interface gkt-theme 'Adwaita-dark'
+kvantummanager --set "catppuccin-mocha-blue"
+
